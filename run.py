be76a07649f0220768dd71a5f64c91a36cf44afa
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-07-29 18:05:45 +0200

    Add code to connect APIs to worksheet and to make sure it works

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
-# python code goes here
+# Import Credentials and worksheet for Google Drive API.
+import gspread
+from google.oauth2.service_account import Credentials
+# Code used from Code Institutes love-sandwiches project. 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('cave_story')
+
+finished_game = SHEET.worksheet('finished_game')
+
+data = finished_game.get_all_values()
+
+print(data)
