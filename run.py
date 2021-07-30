0b6bbb272973d37b0f272e0e45bf1bcf6ae95b0f
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-07-30 15:39:29 +0200

    Add function for secondPathChoice

--- run.py
+++ run.py
@@ -25,13 +25,36 @@ def intro():
     print(f"In the deep Veryovkina caves in Abkhazia year 1545 the wizard {name} is looking for the legendary artefact caldun.")
     print(f"{name} comes to a crossroads. The left path is dark.")
     print("The right path is light.")
-    path = input("Which path will you take? (1 or 2)\n")
-    if path == '1':
-        firstPath()
-    elif path == '2':
-        secondPath()
+    print("Which path will you take?")
+    print("1. Go left")
+    print("2. Go left")
+    pathChoice = input(" (1, 2)\n")
+    if pathChoice == "1":
+        path1()
+    elif pathChoice == "2":
+        path2()
         
-    return path
+    return pathChoice
+
+
+def path1():
+    print(f"{name} starts walking down the dark path, the feeling of unease creeps closer as the air is getting thinner.")
+    print("The flame on the torch is fading  slowly and suddenly is put out.")
+    print(f"{name} can no longer see, only hear the small drops in the distance and smell the wierd odur.")
+    print("What will you do?")
+    print("1. Don't light the torch and keep walking")
+    print("2. Light the torch with two rocks")
+    print("3. Light the torch with magic")
+    secondPathChoice = input("1, 2, 3\n")
+    if secondPathChoice == "1":
+        path1_1()
+    elif secondPathChoice == "2":
+        path1_2()
+    elif secondPathChoice == "3":
+        path1_3()
+
+    return secondPathChoice
+
 
 intro()
 
