015e689406f272035b0ec3e501b6274eefc90557
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-07-30 14:07:07 +0200

    Add first function to print intro for story

--- run.py
+++ run.py
@@ -1,20 +1,30 @@
 # Import Credentials and worksheet for Google Drive API.
 import gspread
 from google.oauth2.service_account import Credentials
-# Code used from Code Institutes love-sandwiches project. 
+
+# Code used from Code Institutes love-sandwiches project.
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+    "https://www.googleapis.com/auth/drive",
+]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('cave_story')
+SHEET = GSPREAD_CLIENT.open("cave_story")
 
-finished_game = SHEET.worksheet('finished_game')
+finished_game = SHEET.worksheet("finished_game")
 
 data = finished_game.get_all_values()
 
-print(data)
+
+name = input("What it your name?\n")
+
+def intro():
+    print(f"In the deep Veryovkina caves in Abkhazia year 1545 the wizard {name} is looking for the legendary artefact caldun.")
+    print(f"{name}s comes to a crossroads. The left path is dark.")
+    print("The right path is light. Which path will you take?")
+
+
+intro()
