968975ead932b2e07fc65fc922b4487230de5102
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-08-09 15:21:35 +0200

    Add a function for selecting a class and name and then return it

--- character.py
+++ character.py
@@ -8,5 +8,25 @@ class character:
         self.mind = mind
         self.name = name
 
+def choseClass():
+    print("1. Wizard/ 2. Warrior/ 3. Elf")
+    characterSelect = int(input("1/2/3\n"))
+    if characterSelect == 1:
+        health = 50
+        mana = 100
+        mind = 50
+    elif characterSelect == 2:
+        health = 100
+        mana = 25
+        mind = 50
+    elif characterSelect == 3:
+        health = 25
+        mana = 50
+        mind = 100
+
+    name = input("What is your name?\n")
+
+    return(health, mana, mind, name)
+        
 
 
