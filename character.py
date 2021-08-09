6b4166a21a572889995fb126685e5a3ff1480ded
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-08-09 15:15:07 +0200

    Add new file for character creation and add class for health, mana, mind and name

---
+++ character.py
@@ -0,0 +1,12 @@
+from pprint import pprint
+
+class character:
+
+    def __init__(self, health, mana, mind, name):
+        self.health = health
+        self.mana = mana
+        self.mind = mind
+        self.name = name
+
+
+
