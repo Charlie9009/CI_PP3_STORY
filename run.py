62af29010d547664447ee01dc0143025cca92fd4
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-08-16 17:31:37 +0200

    Add docstring to run.py intro function

--- run.py
+++ run.py
@@ -6,17 +6,26 @@ from elf_story import elfPath0
 
 
 def intro():
+    """
+    Function to display three choices to the user,
+    Wizard, Warrior or Elf.
+    Opening chosen file when user has chosen class,
+    format NAME to keep user name and new function to continue story.
+    """
     if characterSelect == 1:
+        # Wizard
         with open('stories/wizard/wizard_intro.txt') as f:
             intro = f.read()
             print(intro.format(NAME))
             wizardPath0()
     elif characterSelect == 2:
+        # Warrior
         with open('stories/warrior/warrior_intro.txt') as f:
             intro = f.read()
             print(intro.format(NAME))
             warriorPath0()
     elif characterSelect == 3:
+        # Elf
         with open('stories/elf/elf_intro.txt') as f:
             intro = f.read()
             print(intro.format(NAME))
